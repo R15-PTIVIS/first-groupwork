8e0e2eda3b0a22110927dce8c79d622387c337eb
Kirill Nikolaev <nikolaeff.k.a@gmail.com>
2023-12-10 17:16:29 +0200

    ERR-70 show all cards in the gallery

--- group_work_one/joulukortti/urls.py
+++ group_work_one/joulukortti/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('canvas/', views.canvas, name='canvas')
+    path('canvas/', views.canvas, name='canvas'),
+    path('gallery/', views.gallery, name='gallery')
 ]
--- group_work_one/joulukortti/views.py
+++ group_work_one/joulukortti/views.py
@@ -2,8 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from joulukortti.models import XmasCard
+
+
 def home(request):
     return render(request, 'joulukortti/home.html')
 
 def canvas(request):
     return render(request, 'joulukortti/canvas.html')
+
+def gallery(request):
+    xmas_cards = XmasCard.objects.all()
+    return render(request, 'joulukortti/gallery.html', {'xmas_cards': xmas_cards})
