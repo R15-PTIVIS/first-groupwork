dcbb4515bd9b0af989e8f3a3539ce32214aa5d5e
Kirill Nikolaev <nikolaeff.k.a@gmail.com>
2023-12-10 16:23:29 +0200

    ERR-55 create card model and run migration

--- group_work_one/joulukortti/models.py
+++ group_work_one/joulukortti/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class XmasCard(models.Model):
+    name = models.CharField(max_length=255)
+    date_created = models.DateTimeField(auto_now_add=True)
+    drawing_url = models.CharField(max_length=255, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}"
