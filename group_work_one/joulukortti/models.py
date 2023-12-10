dee053a2626f3b410f03ba6c47879e180273251d
Kirill Nikolaev <nikolaeff.k.a@gmail.com>
2023-12-10 19:33:19 +0200

    ERR-47 rewrite model to save images through SQLite in project directory

--- group_work_one/joulukortti/models.py
+++ group_work_one/joulukortti/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class XmasCard(models.Model):
     name = models.CharField(max_length=255)
     date_created = models.DateTimeField(auto_now_add=True)
-    drawing_url = models.CharField(max_length=255, blank=True, null=True)
+    drawing = models.ImageField(upload_to='xmas_cards/', null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
