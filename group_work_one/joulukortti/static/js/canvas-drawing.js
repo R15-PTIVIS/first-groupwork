353d151d371c81c14a3845c6c6d3ed0d3afcf60b
Toni T Saari <54812574+ToniTSaari@users.noreply.github.com>
2023-12-13 17:02:05 +0200

    TS-13.12.23-03

--- group_work_one/joulukortti/static/js/canvas-drawing.js
+++ group_work_one/joulukortti/static/js/canvas-drawing.js
@@ -4,11 +4,18 @@ const canvas = document.getElementById('drawingCanvas');
 const ctx = canvas.getContext('2d');
 
 // Set up initial drawing properties
-size = 3;
+size = 1;
 ctx.lineCap = 'round';
 ctx.strokeStyle = '#000';
 
 let isDrawing = false;
+
+//stores draw events as a stack
+let drawStack = [];
+//stores individual draw event as a stack
+let drawInstance = [];
+
+var mousePosition = {x:0,y:0}
 
 document.querySelectorAll('.lineWidthBtn').forEach((button) => {
     button.addEventListener('click', () => {
@@ -17,27 +24,58 @@ document.querySelectorAll('.lineWidthBtn').forEach((button) => {
     });
 });
 
+document.querySelector('#undoButton').addEventListener('click', () => {
+    drawStack.splice(-1, 1);
+    draw();
+});
+
 canvas.addEventListener('mousedown', (e) => {
-  isDrawing = true;
-  draw(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+    isDrawing = true;
+    previous = { x: mousePosition.x, y: mousePosition.y };
+    mousePosition = { x: e.clientX - canvas.offsetLeft, y: e.clientY - canvas.offsetTop }
+    //resets previous draw event
+    drawInstance = [];
+    //initiates this draw instance into the drawInstance stack
+    drawInstance.push({ x: mousePosition.x, y: mousePosition.y });
 });
 
 canvas.addEventListener('mousemove', (e) => {
-  if (isDrawing) {
-    draw(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
-  }
+    if (isDrawing)
+    {
+        previous = { x: mousePosition.x, y: mousePosition.y };
+        mousePosition = { x: e.clientX - canvas.offsetLeft, y: e.clientY - canvas.offsetTop }
+        drawInstance.push({ x: mousePosition.x, y: mousePosition.y });
+        //continually draws on canvas
+        ctx.beginPath();
+        ctx.moveTo(previous.x, previous.y);
+        ctx.lineTo(mousePosition.x, mousePosition.y);
+        ctx.stroke();
+    }
 });
 
 canvas.addEventListener('mouseup', () => {
-  isDrawing = false;
-  ctx.beginPath();
+    isDrawing = false;
+    //pushes this drawInstance stack into the overall drawStack
+    drawStack.push(drawInstance);
 });
 
-function draw(x, y) {
+function draw()
+{
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.lineWidth = size;
-  ctx.lineTo(x, y);
-  ctx.stroke();
-  ctx.beginPath();
-  ctx.moveTo(x, y);
+    drawStack.forEach(draw =>
+    {
+        //begins drawing from the first coordinates on the drawStack
+        ctx.beginPath();
+        ctx.moveTo(draw[0].x, draw[0].y);
+
+        //iterates through the drawStack
+        for (let i = 1; i < draw.length; i++)
+        {
+            ctx.lineTo(draw[i].x, draw[i].y);
+        }
+        
+        ctx.stroke();
+    })
 }
 
