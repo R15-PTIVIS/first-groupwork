73b3a64924117acd1df3b3ca47562a52c9cf691f
Toni T Saari <54812574+ToniTSaari@users.noreply.github.com>
2023-12-13 15:13:20 +0200

    TS-13.12.23

--- group_work_one/joulukortti/static/js/canvas-drawing.js
+++ group_work_one/joulukortti/static/js/canvas-drawing.js
@@ -4,11 +4,18 @@ const canvas = document.getElementById('drawingCanvas');
 const ctx = canvas.getContext('2d');
 
 // Set up initial drawing properties
-ctx.lineWidth = 3;
+size = 3;
 ctx.lineCap = 'round';
 ctx.strokeStyle = '#000';
 
 let isDrawing = false;
+
+document.querySelectorAll('.lineWidthBtn').forEach((button) => {
+    button.addEventListener('click', () => {
+        size = button.dataset.size;
+        canvas.getContext('2d').lineWidth = size;
+    });
+});
 
 canvas.addEventListener('mousedown', (e) => {
   isDrawing = true;
@@ -27,6 +34,7 @@ canvas.addEventListener('mouseup', () => {
 });
 
 function draw(x, y) {
+    ctx.lineWidth = size;
   ctx.lineTo(x, y);
   ctx.stroke();
   ctx.beginPath();
