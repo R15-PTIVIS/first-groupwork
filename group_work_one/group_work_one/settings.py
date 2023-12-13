c30b91a0fbec642a0d42368294bc8c408c6d4047
otto2755 <ottog20@gmail.com>
2023-12-13 12:56:55 +0200

    Login, Logout ja Register
    tällä kertaa oikeasti

--- group_work_one/group_work_one/settings.py
+++ group_work_one/group_work_one/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'joulukortti'
+    'joulukortti',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# My settings
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
